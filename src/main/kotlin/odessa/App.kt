1e55fb5a8086789c7a60e311b4109bb4f7b9ff77
Christoph Pickl <c.pickl@tbauctions.com>
2022-01-10 21:00:40 +0100

    add ktor server

--- src/main/kotlin/odessa/App.kt
+++ src/main/kotlin/odessa/App.kt
@@ -1,28 +1,55 @@
 package odessa
 
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 import odessa.templates.Event
 import odessa.templates.TestData
 import odessa.templates.TestTemplate
 
 object App {
+    private val port = 8080
     @JvmStatic
     fun main(args: Array<String>) {
-        val engine = TemplateEngineFactory().build()
-
-        val output = TestTemplate.merge(
-            engine, TestData(
-                username = "Yana",
-                events = listOf(
-                    Event(
-                        name = "Ecstatic Boat 1",
-                        url = "http://odessa.com/1"
-                    )
-                )
-            )
-        )
+        println("Starting webserver at http://localhost:$port")
         
-        println("=============")
-        println(output)
-        println("=============")
+        embeddedServer(Netty, port = port) {
+            val engine = TemplateEngineFactory().build()
+            routing {
+                get("/") {
+                    call.respondText("""
+                        <html>
+                        <body>
+                        <ul>
+                        <li><a href="/test">test</a></li>
+                        </ul>
+                        </body>
+                        </html>
+                    """.trimIndent())
+                }
+                get("/test") {
+                    val output = TestTemplate.merge(
+                        engine, TestData(
+                            username = "Yana",
+                            events = listOf(
+                                Event(
+                                    name = "Ecstatic Boat 1",
+                                    url = "http://odessa.com/1"
+                                )
+                            )
+                        )
+                    )
+                    call.respondText(output, ContentType.Text.Html, HttpStatusCode.OK)
+                }
+            }
+        }.start(wait = true)
+        // TODO shutdown
+        // TODO logging
     }
+    
 }
