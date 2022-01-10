31a8aac773a518fc05202e45540e753db62a9622
Christoph Pickl <c.pickl@tbauctions.com>
2022-01-10 21:09:21 +0100

    make it nicer

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ application {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-netty:1.6.4")
+    implementation("io.ktor:ktor-server-netty:1.6.7")
     implementation("freemarker:freemarker:2.3.9")
+    implementation("ch.qos.logback:logback-classic:1.2.6")
+    implementation("io.github.microutils:kotlin-logging:2.0.6")
 }
--- src/main/kotlin/odessa/App.kt
+++ src/main/kotlin/odessa/App.kt
@@ -16,7 +16,7 @@ object App {
     private val port = 8080
     @JvmStatic
     fun main(args: Array<String>) {
-        println("Starting webserver at http://localhost:$port")
+        println("⭐ ️Starting webserver at: http://localhost:$port ⭐️")
         
         embeddedServer(Netty, port = port) {
             val engine = TemplateEngineFactory().build()
@@ -25,12 +25,13 @@ object App {
                     call.respondText("""
                         <html>
                         <body>
+                        <h1>Available pages:</h1>
                         <ul>
                         <li><a href="/test">test</a></li>
                         </ul>
                         </body>
                         </html>
-                    """.trimIndent())
+                    """.trimIndent(), ContentType.Text.Html, HttpStatusCode.OK)
                 }
                 get("/test") {
                     val output = TestTemplate.merge(
@@ -48,8 +49,8 @@ object App {
                 }
             }
         }.start(wait = true)
+        // TODO use ktor-freemarker plugin: https://ktor.io/docs/freemarker.html#use_template
+        // TODO reload template while server is running
         // TODO shutdown
-        // TODO logging
     }
-    
 }
